ebe7c63d94434454555ec17bedae3b7f4babf170
jte91 <j3sh0n3dward5@gmail.com>
2022-12-23 16:29:07 -0600

    practice environment

---
+++ Coding/C#/COOP/COOP/EntyPoint.cs
@@ -0,0 +1,19 @@
+﻿class Point
+{
+    public int x;
+    public int y;
+}
+
+class EntryPoint
+{
+    static void Main()
+    {
+        Point point = new Point();
+
+        point.x = 5;
+        point.y = 3;
+
+        System.Console.WriteLine(point.x);
+        System.Console.WriteLine(point.y);
+    }
+}
